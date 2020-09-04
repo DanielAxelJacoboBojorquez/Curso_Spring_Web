a25de9219429eebccd69da6dc343ae8ca42dc01a
Daniel Axel Jacobo Bojorquez <46801100+DanielAxelJacoboBojorquez@users.noreply.github.com>
2020-09-04 16:02:15 -0700

    Form MVC Tags

---
+++ src/es/pildoras/spring/mvc/AlumnoController.java
@@ -0,0 +1,16 @@
+package es.pildoras.spring.mvc;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/alumno")
+public class AlumnoController {
+	@RequestMapping("/muestraFormulario")
+	public String muestraFormulario(Model modelo) {
+		Alumno elAlumno=new Alumno();
+		modelo.addAttribute("elAlumno", elAlumno);
+		return "alumnoRegistroFormulario";
+	}
+}
