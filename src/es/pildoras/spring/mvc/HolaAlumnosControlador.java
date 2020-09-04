3d7744a9bf15d2d826a965a13eeaccf814247532
Daniel Axel Jacobo Bojorquez <46801100+DanielAxelJacoboBojorquez@users.noreply.github.com>
2020-09-04 13:53:42 -0700

    Annotation - RequestParam

--- src/es/pildoras/spring/mvc/HolaAlumnosControlador.java
+++ src/es/pildoras/spring/mvc/HolaAlumnosControlador.java
@@ -1,15 +1,12 @@
 package es.pildoras.spring.mvc;
 
-
-import javax.servlet.http.HttpServletRequest;
-
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 
 @Controller
-
 public class HolaAlumnosControlador {
 	@RequestMapping("/muestraFormulario")
 	public String muestraFormulario(){//Proporciona el formulario
@@ -22,8 +19,9 @@ public class HolaAlumnosControlador {
 	}
 	
 	@RequestMapping("/procesarFormulario2")
-	public String otroProcesoFormulario(HttpServletRequest request, Model modelo) {
-		String nombre=request.getParameter("nombreAlumno");
+	//public String otroProcesoFormulario(HttpServletRequest request, Model modelo) {
+	public String otroProcesoFormulario(@RequestParam("nombreAlumno") String nombre, Model modelo) {
+		//String nombre=request.getParameter("nombreAlumno");
 		nombre +=" es el mejor alumno";
 		String mensajeFinal="Quien es el mejor alumno? "+nombre;
 		// Agregando info al modelo
